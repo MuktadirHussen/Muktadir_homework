db6430ee795ed9d707fe2bddc968096db89846c4
MuktadirHussen <muktdrhssn@gmail.com>
2025-10-28 15:07:14 -0400

    Condition

---
+++ src/hw15IfElseCondition/Voter.java
@@ -0,0 +1,28 @@
+package hw15IfElseCondition;
+
+public class Voter {
+
+	public static void main(String[] args) {
+
+		int age = 35;
+
+		if (age == 35) {
+			System.out.println("I am a voter");
+
+		} else if (age < 35) {
+			System.out.println("I am not a voter");
+		}
+
+		else if (age > 35) {
+
+			System.out.println(" I am a Voter from age 18");
+
+		}
+
+		else {
+			System.out.println("Please add a valid age");
+		}
+
+	}
+
+}
