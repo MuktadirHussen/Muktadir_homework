c4f3aa2f5149aeeb0e2d7378eaedf81f417248b9
MuktadirHussen <muktdrhssn@gmail.com>
2025-11-04 21:16:03 -0500

    update

--- src/assignment7Inheritance/AnimalTest.java
+++ src/assignment7Inheritance/AnimalTest.java
@@ -1,20 +1,21 @@
 package assignment7Inheritance;
 
-public class AnimailTest {
+public class AnimalTest {
 
 	public static void main(String[] args) {
 
 		Animal animal = new Animal();
+
 		animal.animalInfo();
 
-		System.out.println();
+		System.out.println("*********************");
 
-		// Single Inheritance
+		// Hierarchical inheritance
 		Reptile reptile = new Reptile();
 		reptile.reptileInfo();
 		reptile.animalInfo();
 
-		System.out.println();
+		System.out.println("*********************");
 
 		// Multi-Level Inheritance
 		Snake snake = new Snake();
@@ -22,42 +23,41 @@ public class AnimailTest {
 		snake.reptileInfo();
 		snake.animalInfo();
 
-		System.out.println();
-		// Hierarchical Inheritance
+		System.out.println("*********************");
+		// Multi-Level Inheritance
 		Cobra cobra = new Cobra();
 		cobra.cobralInfo();
 		cobra.snakeInfo();
 		cobra.reptileInfo();
 		cobra.animalInfo();
 
-		System.out.println();
+		System.out.println("*********************");
 
 		Birds birds = new Birds();
 		birds.birdslInfo();
 		birds.animalInfo();
 
-		System.out.println();
-
+		System.out.println("*********************");
+		// Hierarchical inheritance
 		Robin robin = new Robin();
 		robin.robinInfo();
 		robin.birdslInfo();
 		robin.animalInfo();
 
-		System.out.println();
-
+		System.out.println("*********************");
+		// Hierarchical inheritance
 		Mammal mammal = new Mammal();
 		mammal.mammallInfo();
 		mammal.animalInfo();
 
-		System.out.println();
-		// Multi-Level Inheritance
+		System.out.println("*********************");
+		// Single Inheritance
 		Dog dog = new Dog();
 		dog.doglInfo();
 		dog.mammallInfo();
-		dog.animalInfo();
 
-		System.out.println();
-		// Hierarchical Inheritance
+		System.out.println("*********************");
+		// Multi-Level Inheritance
 		BullDog bullDog = new BullDog();
 		bullDog.bullDogInfo();
 		bullDog.doglInfo();
--- src/assignment7Inheritance/Dog.java
+++ src/assignment7Inheritance/Dog.java
@@ -9,3 +9,4 @@ public class Dog extends Mammal {
 	}
 
 }
+
